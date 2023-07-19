b7d8f1aa1344316cab303f32fafe5d3e435bbdb8
drawerbotnet <139976830+drawerbotnet@users.noreply.github.com>
2023-07-19 19:31:15 +0300

    Update ls.py

--- ls.py
+++ ls.py
@@ -37,7 +37,7 @@ headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537
 ).text
 
 #bypassing
-os.system(clear)
+os.system('cls' if os.name == 'nt' else 'clear')
 print("╦═╗┌─┐┌─┐┌─┐┌┬┐")
 print("╠╦╝├┤ ├─┤│   │ ")
 print("╩╚═└─┘┴ ┴└─┘ ┴ ")
